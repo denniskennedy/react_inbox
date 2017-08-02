e2f864f022d45ef8172edecc926f18b1db4f5166
localadmin <localadmin@localadmins-iMac-2.local>
2017-08-02 15:34:44 -0500

    Added labels and checkbox functionality

--- react-inbox/src/Message.js
+++ react-inbox/src/Message.js
@@ -3,68 +3,82 @@ import React, { Component } from 'react';
 class Message extends  Component{
     constructor(props) {
         super(props)
-        this.state = {
-            selected: this.props.my_message.selected,
-            read: this.props.my_message.read,
-            starred: this.props.my_message.starred
-        }
-
         let isSelected = this.props.my_message.selected
         let isRead = this.props.my_message.read
-        let isStarred = this.props.my_message.starred
+        let isLabeled = this.props.my_message.labels
+
+
+
+        this.state = {
+            selected: isSelected,
+            read: isRead,
+            label: isLabeled
+        }
+
+
 
     }
     render(){
-        if(this.state.selected){
 
-            return(
-                <div className="row message read {isSelected}">
-                    <div className="col-xs-1">
-                        <div className="row">
-                            <div className="col-xs-2">
-                                <input type="checkbox" checked="checked" />
-                            </div>
-                            <div className="col-xs-2">
-                                <i className="star fa fa-star-o"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xs-11">
-                        <a href="#">
-                            <p>{this.props.my_message.subject}</p>
-                        </a>
-                    </div>
-                </div>
-            )
-        }
         if(this.props.my_message.read){
-            return(
-                <div className="row message read">
-                    <div className="col-xs-1">
-                        <div className="row">
-                            <div className="col-xs-2">
-                                <input type="checkbox" />
-                            </div>
-                            <div className="col-xs-2">
-                                <i className="star fa fa-star-o"></i>
+            if(this.state.selected){
+                return(
+                    <div className="row message read selected">
+                        <div className="col-xs-1">
+                            <div className="row">
+                                <div className="col-xs-2">
+                                    <input type="checkbox" checked="checked" />
+                                </div>
+                                <div className="col-xs-2">
+                                    <i className="star fa fa-star-o"></i>
+                                </div>
                             </div>
                         </div>
+                        <div className="col-xs-11">
+                            <span className="label label-warning">{this.props.my_message.labels[0]}</span>
+                            <span className="label label-warning">{this.props.my_message.labels[1]}</span>
+                            <a href="#">
+                                {this.props.my_message.subject}
+                            </a>
+                        </div>
                     </div>
-                    <div className="col-xs-11">
-                        <a href="#">
-                            <p>{this.props.my_message.subject}</p>
-                        </a>
+                )
+            }
+            else {
+                return(
+                    <div className="row message read">
+                        <div className="col-xs-1">
+                            <div className="row">
+                                <div className="col-xs-2">
+                                    <input type="checkbox"/>
+                                </div>
+                                <div className="col-xs-2">
+                                    <i className="star fa fa-star-o"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xs-11">
+                            <span className="label label-warning">{this.props.my_message.labels[0]}</span>
+                            <span className="label label-warning">{this.props.my_message.labels[1]}</span>
+                            <a href="#">
+                                {this.props.my_message.subject}
+                            </a>
+                        </div>
                     </div>
-                </div>
 
-            );
-        }else {
+                );
+
+            }
+
+        }else if (this.state.selected) {
             return(
-                <div className="row message unread">
+
+                <div className="row message unread selected">
+
                     <div className="col-xs-1">
                         <div className="row">
                             <div className="col-xs-2">
-                                <input type="checkbox" />
+                                <input type="checkbox" checked="checked"/>
                             </div>
                             <div className="col-xs-2">
                                 <i className="star fa fa-star-o"></i>
@@ -72,13 +86,39 @@ class Message extends  Component{
                         </div>
                     </div>
                     <div className="col-xs-11">
+                        <span className="label label-warning">{this.props.my_message.labels[0]}</span>
+                        <span className="label label-warning">{this.props.my_message.labels[1]}</span>
                         <a href="#">
-                            <p>{this.props.my_message.subject}</p>
+                            {this.props.my_message.subject}
                         </a>
                     </div>
                 </div>
                 );
 
+        } else {
+            return (
+            <div className="row message unread">
+                <div className="col-xs-1">
+                    <div className="row">
+                        <div className="col-xs-2">
+                            <input type="checkbox" />
+                        </div>
+                        <div className="col-xs-2">
+                            <i className="star fa fa-star-o"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-xs-11">
+                    <span className="label label-warning">{this.props.my_message.labels[0]}</span>
+                    <span className="label label-warning">{this.props.my_message.labels[1]}</span>
+                    <a href="#">
+
+                        {this.props.my_message.subject}
+                    </a>
+                </div>
+            </div>
+            )
         }
 
     }
