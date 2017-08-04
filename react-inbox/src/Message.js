ae43b29e378d340e89605caa8d38d018718be21d
localadmin <localadmin@localadmins-iMac-2.local>
2017-08-04 15:44:54 -0500

    fix starred for inbox

--- react-inbox/src/Message.js
+++ react-inbox/src/Message.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react';
 
 class Message extends  Component{
+
     constructor(props) {
         super(props)
         let isSelected = this.props.my_message.selected
         let isRead = this.props.my_message.read
         let isLabeled = this.props.my_message.labels
-
+        let isStarred = this.props.my_message.starred
 
 
         this.state = {
             selected: isSelected,
             read: isRead,
-            label: isLabeled
+            label: isLabeled,
+            starred: isStarred,
         }
 
 
 
     }
-    render(){
+    getStarred(){
+        if(this.props.my_message.starred){
+            return "star fa fa-star"
 
+        }else {
+            return "star fa fa-star-o"
+        }
+    }
+
+    getRead(){
         if(this.props.my_message.read){
+            return "read"
+
+        }else {
+            return "unread"
+        }
+    }
+
+    getSelected() {
+        if(this.props.my_message.selected){
+            return "selected"
+        }else{
+            return ""
+        }
+    }
+    render(){
             if(this.state.selected){
                 return(
-                    <div className="row message read selected">
+                    <div className={"row message "+ this.getRead() + " " + this.getSelected()}>
                         <div className="col-xs-1">
                             <div className="row">
                                 <div className="col-xs-2">
                                     <input type="checkbox" checked="checked" />
                                 </div>
                                 <div className="col-xs-2">
-                                    <i className="star fa fa-star-o"></i>
+
+                                    <i className= {this.getStarred()}></i>
+
                                 </div>
                             </div>
                         </div>
@@ -46,14 +73,14 @@ class Message extends  Component{
             }
             else {
                 return(
-                    <div className="row message read">
+                    <div className={"row message "+ this.getRead() + " " + this.getSelected()}>
                         <div className="col-xs-1">
                             <div className="row">
                                 <div className="col-xs-2">
                                     <input type="checkbox"/>
                                 </div>
                                 <div className="col-xs-2">
-                                    <i className="star fa fa-star-o"></i>
+                                    <i className= {this.getStarred()}></i>
                                 </div>
                             </div>
                         </div>
@@ -69,60 +96,9 @@ class Message extends  Component{
                 );
 
             }
-
-        }else if (this.state.selected) {
-            return(
-
-                <div className="row message unread selected">
-
-                    <div className="col-xs-1">
-                        <div className="row">
-                            <div className="col-xs-2">
-                                <input type="checkbox" checked="checked"/>
-                            </div>
-                            <div className="col-xs-2">
-                                <i className="star fa fa-star-o"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xs-11">
-                        <span className="label label-warning">{this.props.my_message.labels[0]}</span>
-                        <span className="label label-warning">{this.props.my_message.labels[1]}</span>
-                        <a href="#">
-                            {this.props.my_message.subject}
-                        </a>
-                    </div>
-                </div>
-                );
-
-        } else {
-            return (
-            <div className="row message unread">
-                <div className="col-xs-1">
-                    <div className="row">
-                        <div className="col-xs-2">
-                            <input type="checkbox" />
-                        </div>
-                        <div className="col-xs-2">
-                            <i className="star fa fa-star-o"></i>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="col-xs-11">
-                    <span className="label label-warning">{this.props.my_message.labels[0]}</span>
-                    <span className="label label-warning">{this.props.my_message.labels[1]}</span>
-                    <a href="#">
-
-                        {this.props.my_message.subject}
-                    </a>
-                </div>
-            </div>
-            )
         }
-
     }
-}
+
 
 
 export default Message;
